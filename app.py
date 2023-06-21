83b6620f83116899bdca10833d14562a543ec8c6
ROCKY-MAGIC <136767600+ROCKY-MAGIC@users.noreply.github.com>
2023-06-21 06:12:42 +0000

    Created gmail login

--- app.py
+++ app.py
@@ -1,13 +1,46 @@
-from flask import Flask,render_template
-
+import os
+from flask import Flask,render_template,url_for,redirect,session
+from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
 
+app.secret_key = os.environ['secret']
+
+oauth = OAuth(app)
+
+oauth.register(
+    'google',
+    client_id = os.environ['id'],
+    client_secret = os.environ['secret'],
+server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid profile email https://www.googleapis.com/auth/user.gender.read'}
+)
 
 @app.route("/")
 def hello():
-  return render_template('home.html')
+    profile = dict(session)
+    try:
+     name = profile['user']['userinfo']['name']
+     profile
+     return render_template("home.html", name=name)
+    except:
+     return render_template("home.html",name=None) 
 
+    
+@app.route('/login')
+def login():
+    redirect_uri = url_for('authorize', _external=1)
+    redirect_uri = "https"+redirect_uri[4:]
+    print(redirect_uri)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+
+@app.route('/authorize')
+def authorize():
+    token = oauth.google.authorize_access_token()
+    
+    session['user'] = token
+    return redirect('/')
 
 
 if __name__ == "__main__":
