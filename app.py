1ef6f970879cf14a97e7ef0766b4ae32d7255f53
ROCKY-MAGIC <136767600+ROCKY-MAGIC@users.noreply.github.com>
2023-06-29 05:33:56 +0000

    Added database

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask,render_template,url_for,redirect,session
+from flask import Flask, render_template, url_for, redirect, session,request,jsonify
 from authlib.integrations.flask_client import OAuth
+
+from db import *
 
 app = Flask(__name__)
 
@@ -9,46 +11,92 @@ app.secret_key = os.environ['secret']
 oauth = OAuth(app)
 
 oauth.register(
-    'google',
-    client_id = os.environ['id'],
-    client_secret = os.environ['secret'],
-server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    client_kwargs={'scope': 'openid profile email https://www.googleapis.com/auth/user.gender.read'}
-)
+  'google',
+  client_id=os.environ['id'],
+  client_secret=os.environ['secret'],
+  server_metadata_url=
+  'https://accounts.google.com/.well-known/openid-configuration',
+  client_kwargs={
+    'scope':
+    'openid profile email https://www.googleapis.com/auth/user.gender.read'
+  })
+
 
 @app.route("/")
 def hello():
-    profile = dict(session)
-    try:
-     name = profile['user']['userinfo']['name']
-     photo = profile['user']['userinfo']['picture']
+  profile = dict(session)
+  try:
+    name = profile['user']['userinfo']['name']
+    photo = profile['user']['userinfo']['picture']
+    id = profile['user']['userinfo']['email']
+    head = profile['id']
+    return render_template("mylist.html", name=name, photo=photo,  title = head)
 
-     return render_template("home.html", name=name,links="logout",dynamic="logout",photo=photo)
-    except:
-     print(url_for("login"))
-     return render_template("home.html",dynamic="login",links="login",name="None",photo="static/20230616_144154.png") 
-      
+  except:
+    return render_template("home.html", photo="static/20230616_144154.png")
+
+
 @app.route("/logout")
-def logout ():
-  session.pop("user",None)
-  return redirect ("/")
+def logout():
+  session.pop("user", None)
+  return redirect("/")
 
-    
+@app.post("/add_todo")
+def add():
+  try:
+    profile = dict(session)
+    id = profile['user']['userinfo']['email']
+    id = id[:id.find("@")]
+    title = request.form.get("title").rstrip()
+    add_todo(id, title)
+    return redirect("/user")
+
+  except :
+    return redirect("/home")
+
+
+def page_not_found(e):
+    return render_template('404.html',photo="static/20230616_144154.png"), 404
+
 @app.route('/login')
 def login():
-    redirect_uri = url_for('authorize', _external=1)
-    #redirect_uri = "https"+redirect_uri[4:]
-    print(redirect_uri)
-    return oauth.google.authorize_redirect(redirect_uri)
+  redirect_uri = url_for('authorize', _external=1)
+  redirect_uri = "https" + redirect_uri[4:]
+  print(redirect_uri)
+  return oauth.google.authorize_redirect(redirect_uri)
+
+@app.route('/test')
+def test():
+  con.execute(text("SET GLOBAL  time_zone = '+05:30';"))
+  time = list(con.execute(text("SELECT created FROM velraj0004;")))[0][0]
+  return jsonify(time)
+
+@app.get('/user')
+def user():
+  try:
+    profile = dict(session)
+    id = profile['user']['userinfo']['email']
+    id = id[:id.find("@")]
+    detail = []
+    for i in list(access_tit(id)):
+      detail.append(i[0])
+
+    session['id'] = detail
+    print(session)
+    return redirect(url_for("hello"))
+  except:
+    return redirect(url_for("hello"))
 
 
 @app.route('/authorize')
 def authorize():
-    token = oauth.google.authorize_access_token()
-    
-    session['user'] = token
-    return redirect('/')
+  token = oauth.google.authorize_access_token()
+
+  session['user'] = token
+  return redirect('/user')
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",debug=1)
+  
+  app.register_error_handler(404, page_not_found)
+  app.run(host="0.0.0.0", debug=1)
--- db.py
+++ db.py
@@ -1,15 +1,100 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, text
 import os
 
-connection_string = my_secret = os.environ['Conn_str']
-engine = create_engine(connection_string, 
-                      connect_args={
-          "ssl":{
-            "ssl_ca": "/etc/ssl/cert.pem"
-          }
-                      })
+connection_string = os.environ['Conn_str']
+engine = create_engine(connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": os.environ['doc']
+                       }})
 
-with engine.connect() as connection:
-  result = connection.execute(text("SELECT * FROM notepad;"))
+con = engine.connect()
+con.execute(text("SET GLOBAL  time_zone = '+05:30';"))
 
-print(type(result.all()))
+def viewer(id, title):
+  result = con.execute(
+    text(f"SELECT * FROM {id} WHERE title = '{title}';")).all()
+  print(result)
+
+
+def add_user(id):
+  con.execute(
+    text(
+      f"CREATE TABLE {id}(title VARCHAR(50) NOT NULL UNIQUE, description varchar(500),created datetime,modified datetime, completed bit,PRIMARY KEY(title));"
+    ))
+
+
+def add_todo(id, title):
+  try:
+    con.execute(
+      text(
+        f"INSERT INTO {id}(title,created,completed) VALUES('{title}',NOW(),1)")
+    )
+
+  except:
+    return 0
+
+
+def fetch_det(id):
+  det = con.execute(text(f"SELECT title FROM {id};")).all()
+  return det
+
+
+def access_tit(id):
+  try:
+    txt = fetch_det(id)
+    return txt
+
+  except:
+    add_user(id)
+    txt = dict(fetch_det(id))
+    return txt
+
+
+def modify(id, col, txt, title):
+  con.execute(
+    text(
+      f"UPDATE {id} SET {col} = '{txt}',modified = NOW() WHERE title ='{title}'"
+    ))
+  con.execute(text("commit"))
+
+
+def delete(id, title):
+  con.execute(text(f"DELETE FROM {id} WHERE title = '{title}';"))
+
+
+def comp(id, title):
+  stat = not bool(
+    con.execute(text(
+      f"SELECT completed FROM {id} WHERE title = '{title}';")).all()[0][0][-1])
+  print(stat)
+
+  con.execute(
+    text(f"UPDATE {id} SET completed = {stat} WHERE title = '{title}';"))
+
+def conv(t):
+  fin = ""
+  bc = [int(i) for i in t[11:].split(":")]
+  fin += t[:11]
+  
+  bc[0] = bc[0]+5
+  st = not bc[0] >= 24
+  bc[0] = bc[0] if st else bc[0]-24
+  bc[1] = bc[1]+30
+  st = not bc[1] >= 60
+  bc[1] = bc[1] if st else bc[1]-60
+  
+  bc = [str(i) for i in bc]
+  fin += ":".join(bc)+","
+  return fin
+
+def time(id, title):
+ fin = ""
+ for i in {"created","modified"}:
+  t = str(con.execute(text(f"SELECT {i} FROM {id} WHERE title = '{title}'")).one()[0])
+  
+  try:
+   fin += conv(t)
+  except :
+   return fin
+  return fin
+
